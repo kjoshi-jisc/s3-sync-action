6d18fcdffe65aa27a37191e3af45bf6bc3df0e20
Kiran Joshi <kiran.joshi@jisc.ac.uk>
2020-05-28 16:43:10 +0100

    Update action and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-if [ -z "$AWS_S3_BUCKET" ]; then
-  echo "AWS_S3_BUCKET is not set. Quitting."
+if [ -z "$AWS_S3_SOURCE_BUCKET" ]; then
+  echo "AWS_S3_SOURCE_BUCKET is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_S3_DESTINATION_BUCKET" ]; then
+  echo "AWS_S3_DESTINATION_BUCKET is not set. Quitting."
   exit 1
 fi
 
@@ -19,7 +24,7 @@ fi
 
 # Default to us-east-1 if AWS_REGION not set.
 if [ -z "$AWS_REGION" ]; then
-  AWS_REGION="us-east-1"
+  AWS_REGION="eu-west-1"
 fi
 
 # Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
@@ -39,7 +44,7 @@ EOF
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+sh -c "aws s3 sync s3://${AWS_S3_SOURCE_BUCKET} s3://${AWS_S3_DESTINATION_BUCKET} \
               --profile s3-sync-action \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
